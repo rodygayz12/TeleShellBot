210fd0b09913aef80b7290cfbafaad9dc01d22b7
rodygayz12 <161971854+rodygayz12@users.noreply.github.com>
2024-04-19 23:09:09 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+const http = require('http');
 const TeleBot = require('telebot');
 const { spawn } = require('child_process');
 
 const config = require('./config').config();
-const { adminUsers } = config;
+const { botToken, adminUsers } = config;
 
-const bot = new TeleBot(config.botToken);
+const bot = new TeleBot(botToken);
+
+// Create an HTTP server to listen on port 8080
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.listen(8080, () => {
+  console.log('Server running at http://localhost:8080');
+});
+
 bot.on('text', (msg) => {
   const { id } = msg.from;
   msg.reply.text('$:' + msg.text);
