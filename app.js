002311d0b420bdbda25d48d9cdce5ce92b59c375
rodygayz12 <161971854+rodygayz12@users.noreply.github.com>
2024-04-19 22:49:04 +0400

    Update and rename app.py to app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const TeleBot = require('telebot');
+
+const config = require('./config').config();
+const { botToken, adminUsers } = config;
+
+const bot = new TeleBot(botToken);
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is running!');
+});
+
+server.listen(8080, () => {
+  console.log('Server running at http://localhost:8080');
+});
+
+bot.on('text', (msg) => {
+  // ... Your existing bot logic here
+});
+
+bot.start();
